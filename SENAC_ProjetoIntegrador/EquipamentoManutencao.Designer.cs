cddee3edde752f121ad3f85aa734e19689a408a8
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-08-07 21:51:05 -0300

    Criação do layout da tela EquipamentoManutenção

--- SENAC_ProjetoIntegrador/EquipamentoManutencao.Designer.cs
+++ SENAC_ProjetoIntegrador/EquipamentoManutencao.Designer.cs
@@ -28,19 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(460, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 25);
+            label1.TabIndex = 0;
+            label1.Text = "TELA DE MANUTENÇÃO";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 49);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1083, 615);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(448, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(195, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Manutenção de [menu]";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(21, 224);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1044, 294);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Pesquisar";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(21, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Incluir";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(768, 540);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 51);
+            button2.TabIndex = 5;
+            button2.Text = "Remover";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 125);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(482, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(920, 540);
+            button3.Name = "button3";
+            button3.Size = new Size(145, 51);
+            button3.TabIndex = 6;
+            button3.Text = "Salvar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(21, 540);
+            button4.Name = "button4";
+            button4.Size = new Size(256, 51);
+            button4.TabIndex = 7;
+            button4.Text = "Nome do Usuário Logado";
+            button4.UseVisualStyleBackColor = true;
             // 
             // EquipamentoManutencao
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1118, 664);
+            ClientSize = new Size(1107, 676);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "EquipamentoManutencao";
             Text = "EquipamentoManutencao";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label3;
+        private DataGridView dataGridView1;
     }
 }
