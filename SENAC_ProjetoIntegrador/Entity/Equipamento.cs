667f74c1a405d9bcf8a44c64484ca5d820cb7563
Puffer Notebook <marceloballinflores@gmail.com>
2025-08-11 20:13:10 -0300

    iniciada a criacao das entidades para gerar o banco

---
+++ SENAC_ProjetoIntegrador/Entity/Equipamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENAC_ProjetoIntegrador.Entity
+{
+    public class Equipamento
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Nome { get; set; }
+        public string Modelo { get; set; } = string.Empty;
+        public int Ano { get; set; }
+        public int CodBarra { get; set; }
+        public int NCM { get; set; }
+        public decimal Custo { get; set; }
+        public decimal Valor { get; set; }
+        public string Lucro { get; set; }
+    }
+}
---
+++ SENAC_ProjetoIntegrador/Entity/OrdemServico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENAC_ProjetoIntegrador.Entity
+{
+    public class OrdemServico
+    {
+        public int Id { get; set; }
+        public string Equipamento { get; set; }
+        public string Modelo { get; set; }
+        public string Cliente { get; set; }
+        public int CpfCnpj { get; set; }
+        public string DescricaoGeral { get; set; }
+        public string DescricaoEncerramento { get; set; }
+        public decimal ValorTotal { get; set; }
+    }
+}
