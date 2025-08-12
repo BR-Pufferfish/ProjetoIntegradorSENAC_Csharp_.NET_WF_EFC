e52fe5ad7be4671d5fcb2036ff34bbfa0134a1ac
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-08-11 21:04:33 -0300

    Criação do Serviço.cs

--- SENAC_ProjetoIntegrador/Entity/Servico.cs
+++ SENAC_ProjetoIntegrador/Entity/Servico.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,18 @@ namespace SENAC_ProjetoIntegrador.Entity
 {
     public class Servico
     {
-        public int
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+
+        public int Ano { get; set; }
+        public decimal Custo { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Lucro => Valor - Custo;
+
+        public int DataInclusao { get; set; }
+        public int CodBarra { get; set; }
+        public int NCM { get; set; }
+        public string Descricao { get; set; }
     }
 }
