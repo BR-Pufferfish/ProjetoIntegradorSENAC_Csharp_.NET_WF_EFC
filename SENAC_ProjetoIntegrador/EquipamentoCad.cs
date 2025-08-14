a4bab20e6a2201ae8f7d91247f2e5c1af58b6f9b
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-08-14 20:50:14 -0300

    Configurando tela EquipamentoCad...

--- SENAC_ProjetoIntegrador/EquipamentoCad.cs
+++ SENAC_ProjetoIntegrador/EquipamentoCad.cs
@@ -13,6 +13,7 @@ namespace SENAC_ProjetoIntegrador
 {
     public partial class EquipamentoCad : Form
     {
+        private Equipamento _equipamento;
         public EquipamentoCad()
         {
             InitializeComponent();
@@ -21,19 +22,64 @@ namespace SENAC_ProjetoIntegrador
 
         public EquipamentoCad(Equipamento equipamento)
         {
+            _equipamento = equipamento;
             InitializeComponent();
 
-            CarredaDadosEquipamento(equipamento);
+            CarregaDadosEquipamento();
         }
 
-        private void CarredaDadosEquipamento(Equipamento equipamento)
+        private void CarregaDadosEquipamento()
         {
-            textBoxNome.Text = equipamento.Nome;
+            if (_equipamento != null)
+            {
+                txtNome.Text = _equipamento.Nome;
+                txtModelo.Text = _equipamento.Modelo;
+                txtAno.MaxLength = 4;
+                txtCodigodeBarra.Text = _equipamento.CodBarra.ToString();
+                txtNCM.Text = _equipamento.NCM.ToString();
+                txtCusto.Text = _equipamento.Custo.ToString("F2");
+                txtValor.Text = _equipamento.Valor.ToString("F2");
+                txtLucro.Text = _equipamento.Lucro.ToString("F2");
+
+            }
+        }
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (_equipamento == null)
+            {
+                InserirEquipamentp();
+            }
+            else
+            {
+                AtualizarEquipamento();
+            }
+        }
+
+        private void AtualizarEquipamento()
+        {
+            using (var banco = new AplicacaoDBContext())
+            {
+                string nome = txtNome.Text;
+                string modelo = txtModelo.Text;
+                int ano = int.Parse(txtAno.Text);
+                int codBarra = int.Parse(txtCodigodeBarra.Text);
+                int nmc = int.Parse(txtNCM.Text);
+                decimal custo = decimal.Parse(txtCusto.Text);
+                decimal valor = decimal.Parse(txtValor.Text);
+                decimal lucro = decimal.Parse(txtLucro.Text);
+
+            }
+        }
+        private void InserirEquipamentp()
+        {
+            throw new NotImplementedException();
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+
     }
 }
