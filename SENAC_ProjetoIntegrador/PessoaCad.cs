02393e733a360990cb350e82281c4a92462f1c35
Puffer Notebook <marceloballinflores@gmail.com>
2025-08-22 19:57:36 -0300

    ajustes

--- SENAC_ProjetoIntegrador/PessoaCad.cs
+++ SENAC_ProjetoIntegrador/PessoaCad.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SENAC_ProjetoIntegrador.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,33 @@ using System.Windows.Forms;
 
 namespace SENAC_ProjetoIntegrador
 {
+    private Pessoa _pessoa;
     public partial class PessoaCad : Form
     {
         public PessoaCad()
         {
             InitializeComponent();
+        }
+
+        public PessoaCad(Pessoa pessoa)
+        {
+            _pessoa = pessoa;
+            InitializeComponent();
+
+            CarregarDadosPessoa();
+        }
+
+        private void CarregarDadosPessoa()
+        {
+            if(_pessoa != null)
+            {
+                txtNome.Text = _pessoa.Nome;
+                txtCpfCnpj.Text = _pessoa.CPF;
+                txtDataNascimento.Text = _pessoa.DataNascimento.ToString("dd/MM/yyyy");
+                txtEmail.Text = _pessoa.Email;
+                txtTelefone.Text = _pessoa.Telefone;
+                txtEndereco.Text = _pessoa.Endereco;
+            }
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
