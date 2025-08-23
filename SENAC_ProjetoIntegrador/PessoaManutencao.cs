865261fd5e459af46abc6a2a33dbf50f97d871b9
PufferDesktop <marceloballinflores@gmail.com>
2025-08-22 23:42:11 -0300

    ajustada a pessoaManutenção visual e codigo

--- SENAC_ProjetoIntegrador/PessoaManutencao.cs
+++ SENAC_ProjetoIntegrador/PessoaManutencao.cs
@@ -19,6 +19,83 @@ namespace SENAC_ProjetoIntegrador
             InitializeComponent();
         }
 
+        private void PessoaManutencao_Load(object sender, EventArgs e)
+        {
+            BuscarPessoa();
+        }
 
+        private void BuscarPessoa()
+        {
+            using (var bd = new AplicacaoDBContext())
+            {
+                var pessoa = bd.Pessoas.AsQueryable();
+                if (!string.IsNullOrEmpty(txtPesquisar.Text))
+                {
+                    pessoa = pessoa.Where(p => p.Nome.ToLower().Contains(txtPesquisar.Text) ||
+                                               p.Cpf_cnpj.ToString().Contains(txtPesquisar.Text) ||
+                                               p.Razaosoc.ToLower().Contains(txtPesquisar.Text));
+                }
+
+                dataGridView1.DataSource = pessoa.ToList();
+            }
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            new PessoaCad().ShowDialog();
+            BuscarPessoa();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtPesquisar_TextChanged(object sender, EventArgs e)
+        {
+            BuscarPessoa();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var pessoaSelecionada = dataGridView1.Rows[e.RowIndex].DataBoundItem as Pessoa;
+                btnEditar.Enabled = true;
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (pessoaSelecionada != null)
+            {
+                var pessoa = new PessoaCad(pessoaSelecionada);
+                pessoa.ShowDialog();
+                BuscarPessoa();
+                pessoaSelecionada = null;
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (pessoaSelecionada != null)
+            {
+                using (var bd = new AplicacaoDBContext())
+                {
+                    bd.Pessoas.Remove(pessoaSelecionada);
+                    bd.SaveChanges();
+                }
+                MessageBox.Show("Pessoa removida com sucesso!", "Sucesso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BuscarPessoa();
+                pessoaSelecionada = null;
+
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma Pessoa para remover.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
     }
 }
