9e97fae7774609c8a1757b56aa40b8c1575a799b
Puffer Notebook <marceloballinflores@gmail.com>
2025-08-26 21:39:32 -0300

    login como primeira tela

--- SENAC_ProjetoIntegrador/Program.cs
+++ SENAC_ProjetoIntegrador/Program.cs
@@ -44,7 +44,7 @@ namespace SENAC_ProjetoIntegrador
 
             // Executa o formulário da aplicação (se necessário)
             ApplicationConfiguration.Initialize();
-            Application.Run(new ServicoManutencao());
+            Application.Run(new Login());
         }
     }
 }
