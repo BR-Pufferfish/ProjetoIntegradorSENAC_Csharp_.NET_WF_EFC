23bfdf6a7fe3a676fe919c8feb7e1fd5f351e1e9
Puffer Notebook <marceloballinflores@gmail.com>
2025-08-28 20:09:54 -0300

    feita nova migration

---
+++ SENAC_ProjetoIntegrador/Migrations/20250828230935_v12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SENAC_ProjetoIntegrador.Migrations
+{
+    /// <inheritdoc />
+    public partial class v12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DtEncerramento",
+                table: "Equipamentos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DtEncerramento",
+                table: "Equipamentos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
