f7260a83499b9e2e753ce7d222110e8317d8ccf4
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-09-01 19:57:00 -0300

    Ajustando a tela de Menu

--- SENAC_ProjetoIntegrador/Menu.Designer.cs
+++ SENAC_ProjetoIntegrador/Menu.Designer.cs
@@ -34,7 +34,6 @@
             btnVenda = new Button();
             btnPecaItem = new Button();
             btnEquipamento = new Button();
-            btnOrdemServico = new Button();
             btnPessoa = new Button();
             btnFechar = new Label();
             pictureBox1 = new PictureBox();
@@ -44,6 +43,10 @@
             pictureBox5 = new PictureBox();
             pictureBox6 = new PictureBox();
             pictureBox7 = new PictureBox();
+            btnOrdemServico = new CuoreUI.Controls.cuiButton();
+            cuiGradientPanel1 = new CuoreUI.Controls.cuiGradientPanel();
+            cuiButton1 = new CuoreUI.Controls.cuiButton();
+            cuiButton2 = new CuoreUI.Controls.cuiButton();
             gpbMenus.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -52,30 +55,35 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            cuiGradientPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // gpbMenus
             // 
-            gpbMenus.Controls.Add(btnRelatorio);
-            gpbMenus.Controls.Add(btnServico);
-            gpbMenus.Controls.Add(btnVenda);
-            gpbMenus.Controls.Add(btnPecaItem);
-            gpbMenus.Controls.Add(btnEquipamento);
+            gpbMenus.BackColor = Color.Transparent;
+            gpbMenus.Controls.Add(pictureBox7);
+            gpbMenus.Controls.Add(cuiButton2);
+            gpbMenus.Controls.Add(cuiButton1);
             gpbMenus.Controls.Add(btnOrdemServico);
-            gpbMenus.Controls.Add(btnPessoa);
+            gpbMenus.Controls.Add(pictureBox6);
+            gpbMenus.Controls.Add(pictureBox5);
+            gpbMenus.Controls.Add(pictureBox4);
+            gpbMenus.Controls.Add(pictureBox3);
+            gpbMenus.Controls.Add(pictureBox2);
+            gpbMenus.Controls.Add(pictureBox1);
             gpbMenus.Font = new Font("Segoe UI", 12F);
             gpbMenus.Location = new Point(12, 11);
             gpbMenus.Margin = new Padding(3, 2, 3, 2);
             gpbMenus.Name = "gpbMenus";
             gpbMenus.Padding = new Padding(3, 2, 3, 2);
-            gpbMenus.Size = new Size(350, 869);
+            gpbMenus.Size = new Size(350, 878);
             gpbMenus.TabIndex = 0;
             gpbMenus.TabStop = false;
             gpbMenus.Text = "Menus";
             // 
             // btnRelatorio
             // 
-            btnRelatorio.Location = new Point(1, 391);
+            btnRelatorio.Location = new Point(394, 509);
             btnRelatorio.Margin = new Padding(3, 2, 3, 2);
             btnRelatorio.Name = "btnRelatorio";
             btnRelatorio.Size = new Size(221, 46);
@@ -86,7 +94,7 @@
             // 
             // btnServico
             // 
-            btnServico.Location = new Point(1, 291);
+            btnServico.Location = new Point(394, 372);
             btnServico.Margin = new Padding(3, 2, 3, 2);
             btnServico.Name = "btnServico";
             btnServico.Size = new Size(221, 46);
@@ -97,7 +105,7 @@
             // 
             // btnVenda
             // 
-            btnVenda.Location = new Point(0, 341);
+            btnVenda.Location = new Point(394, 442);
             btnVenda.Margin = new Padding(3, 2, 3, 2);
             btnVenda.Name = "btnVenda";
             btnVenda.Size = new Size(221, 46);
@@ -108,7 +116,7 @@
             // 
             // btnPecaItem
             // 
-            btnPecaItem.Location = new Point(1, 247);
+            btnPecaItem.Location = new Point(378, 302);
             btnPecaItem.Margin = new Padding(3, 2, 3, 2);
             btnPecaItem.Name = "btnPecaItem";
             btnPecaItem.Size = new Size(221, 46);
@@ -119,7 +127,7 @@
             // 
             // btnEquipamento
             // 
-            btnEquipamento.Location = new Point(1, 202);
+            btnEquipamento.Location = new Point(368, 239);
             btnEquipamento.Margin = new Padding(3, 2, 3, 2);
             btnEquipamento.Name = "btnEquipamento";
             btnEquipamento.Size = new Size(221, 46);
@@ -128,20 +136,9 @@
             btnEquipamento.UseVisualStyleBackColor = true;
             btnEquipamento.Click += btnEquipamento_Click;
             // 
-            // btnOrdemServico
-            // 
-            btnOrdemServico.Location = new Point(1, 115);
-            btnOrdemServico.Margin = new Padding(3, 2, 3, 2);
-            btnOrdemServico.Name = "btnOrdemServico";
-            btnOrdemServico.Size = new Size(221, 46);
-            btnOrdemServico.TabIndex = 3;
-            btnOrdemServico.Text = "Ordem de Serviço";
-            btnOrdemServico.UseVisualStyleBackColor = true;
-            btnOrdemServico.Click += button1_Click;
-            // 
             // btnPessoa
             // 
-            btnPessoa.Location = new Point(1, 158);
+            btnPessoa.Location = new Point(368, 184);
             btnPessoa.Margin = new Padding(3, 2, 3, 2);
             btnPessoa.Name = "btnPessoa";
             btnPessoa.Size = new Size(221, 46);
@@ -166,7 +163,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.usuario_de_perfil;
-            pictureBox1.Location = new Point(391, 151);
+            pictureBox1.Location = new Point(254, 378);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(64, 64);
             pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -176,7 +173,7 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.computador;
-            pictureBox2.Location = new Point(391, 221);
+            pictureBox2.Location = new Point(254, 448);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(64, 64);
             pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -186,7 +183,7 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.servico_tecnico;
-            pictureBox3.Location = new Point(391, 354);
+            pictureBox3.Location = new Point(254, 581);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(64, 64);
             pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -196,7 +193,7 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.engrenagem;
-            pictureBox4.Location = new Point(391, 284);
+            pictureBox4.Location = new Point(254, 511);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(64, 64);
             pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -206,7 +203,7 @@
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.moeda;
-            pictureBox5.Location = new Point(391, 424);
+            pictureBox5.Location = new Point(254, 651);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(64, 64);
             pictureBox5.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -216,7 +213,7 @@
             // pictureBox6
             // 
             pictureBox6.Image = Properties.Resources.plano;
-            pictureBox6.Location = new Point(391, 509);
+            pictureBox6.Location = new Point(254, 736);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(64, 64);
             pictureBox6.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -225,33 +222,164 @@
             // 
             // pictureBox7
             // 
+            pictureBox7.BackColor = Color.White;
             pictureBox7.Image = Properties.Resources.cadastro;
-            pictureBox7.Location = new Point(391, 81);
+            pictureBox7.Location = new Point(39, 104);
             pictureBox7.Name = "pictureBox7";
             pictureBox7.Size = new Size(64, 64);
             pictureBox7.SizeMode = PictureBoxSizeMode.AutoSize;
             pictureBox7.TabIndex = 12;
             pictureBox7.TabStop = false;
             // 
+            // btnOrdemServico
+            // 
+            btnOrdemServico.CheckButton = false;
+            btnOrdemServico.Checked = false;
+            btnOrdemServico.CheckedBackground = Color.FromArgb(255, 106, 0);
+            btnOrdemServico.CheckedForeColor = Color.White;
+            btnOrdemServico.CheckedImageTint = Color.White;
+            btnOrdemServico.CheckedOutline = Color.FromArgb(255, 106, 0);
+            btnOrdemServico.Content = "Ordem de Serviço";
+            btnOrdemServico.DialogResult = DialogResult.None;
+            btnOrdemServico.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnOrdemServico.ForeColor = Color.Black;
+            btnOrdemServico.HoverBackground = Color.White;
+            btnOrdemServico.HoverForeColor = Color.Black;
+            btnOrdemServico.HoverImageTint = Color.White;
+            btnOrdemServico.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            btnOrdemServico.Image = null;
+            btnOrdemServico.ImageAutoCenter = true;
+            btnOrdemServico.ImageExpand = new Point(0, 0);
+            btnOrdemServico.ImageOffset = new Point(0, 0);
+            btnOrdemServico.Location = new Point(6, 98);
+            btnOrdemServico.Name = "btnOrdemServico";
+            btnOrdemServico.NormalBackground = Color.White;
+            btnOrdemServico.NormalForeColor = Color.Black;
+            btnOrdemServico.NormalImageTint = Color.White;
+            btnOrdemServico.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            btnOrdemServico.OutlineThickness = 1F;
+            btnOrdemServico.PressedBackground = Color.WhiteSmoke;
+            btnOrdemServico.PressedForeColor = Color.FromArgb(32, 32, 32);
+            btnOrdemServico.PressedImageTint = Color.White;
+            btnOrdemServico.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            btnOrdemServico.Rounding = new Padding(30);
+            btnOrdemServico.Size = new Size(338, 79);
+            btnOrdemServico.TabIndex = 13;
+            btnOrdemServico.TextAlignment = StringAlignment.Far;
+            btnOrdemServico.TextOffset = new Point(0, 0);
+            btnOrdemServico.Click += button1_Click;
+            // 
+            // cuiGradientPanel1
+            // 
+            cuiGradientPanel1.Controls.Add(gpbMenus);
+            cuiGradientPanel1.Controls.Add(btnPessoa);
+            cuiGradientPanel1.Controls.Add(btnRelatorio);
+            cuiGradientPanel1.Controls.Add(btnEquipamento);
+            cuiGradientPanel1.Controls.Add(btnPecaItem);
+            cuiGradientPanel1.Controls.Add(btnServico);
+            cuiGradientPanel1.Controls.Add(btnVenda);
+            cuiGradientPanel1.GradientAngle = 0F;
+            cuiGradientPanel1.Location = new Point(0, 0);
+            cuiGradientPanel1.Name = "cuiGradientPanel1";
+            cuiGradientPanel1.OutlineThickness = 1F;
+            cuiGradientPanel1.PanelColor1 = Color.DarkOrchid;
+            cuiGradientPanel1.PanelColor2 = Color.Transparent;
+            cuiGradientPanel1.PanelOutlineColor1 = Color.Transparent;
+            cuiGradientPanel1.PanelOutlineColor2 = Color.Transparent;
+            cuiGradientPanel1.Rounding = new Padding(8);
+            cuiGradientPanel1.Size = new Size(843, 903);
+            cuiGradientPanel1.TabIndex = 6;
+            // 
+            // cuiButton1
+            // 
+            cuiButton1.CheckButton = false;
+            cuiButton1.Checked = false;
+            cuiButton1.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton1.CheckedForeColor = Color.White;
+            cuiButton1.CheckedImageTint = Color.White;
+            cuiButton1.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton1.Content = "Ordem de Serviço";
+            cuiButton1.DialogResult = DialogResult.None;
+            cuiButton1.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiButton1.ForeColor = Color.Black;
+            cuiButton1.HoverBackground = Color.White;
+            cuiButton1.HoverForeColor = Color.Black;
+            cuiButton1.HoverImageTint = Color.White;
+            cuiButton1.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton1.Image = null;
+            cuiButton1.ImageAutoCenter = true;
+            cuiButton1.ImageExpand = new Point(0, 0);
+            cuiButton1.ImageOffset = new Point(0, 0);
+            cuiButton1.Location = new Point(6, 183);
+            cuiButton1.Name = "cuiButton1";
+            cuiButton1.NormalBackground = Color.White;
+            cuiButton1.NormalForeColor = Color.Black;
+            cuiButton1.NormalImageTint = Color.White;
+            cuiButton1.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.OutlineThickness = 1F;
+            cuiButton1.PressedBackground = Color.WhiteSmoke;
+            cuiButton1.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton1.PressedImageTint = Color.White;
+            cuiButton1.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.Rounding = new Padding(30);
+            cuiButton1.Size = new Size(338, 79);
+            cuiButton1.TabIndex = 13;
+            cuiButton1.TextAlignment = StringAlignment.Far;
+            cuiButton1.TextOffset = new Point(0, 0);
+            cuiButton1.Click += button1_Click;
+            // 
+            // cuiButton2
+            // 
+            cuiButton2.CheckButton = false;
+            cuiButton2.Checked = false;
+            cuiButton2.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton2.CheckedForeColor = Color.White;
+            cuiButton2.CheckedImageTint = Color.White;
+            cuiButton2.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton2.Content = "Ordem de Serviço";
+            cuiButton2.DialogResult = DialogResult.None;
+            cuiButton2.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiButton2.ForeColor = Color.Black;
+            cuiButton2.HoverBackground = Color.White;
+            cuiButton2.HoverForeColor = Color.Black;
+            cuiButton2.HoverImageTint = Color.White;
+            cuiButton2.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton2.Image = null;
+            cuiButton2.ImageAutoCenter = true;
+            cuiButton2.ImageExpand = new Point(0, 0);
+            cuiButton2.ImageOffset = new Point(0, 0);
+            cuiButton2.Location = new Point(6, 268);
+            cuiButton2.Name = "cuiButton2";
+            cuiButton2.NormalBackground = Color.White;
+            cuiButton2.NormalForeColor = Color.Black;
+            cuiButton2.NormalImageTint = Color.White;
+            cuiButton2.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.OutlineThickness = 1F;
+            cuiButton2.PressedBackground = Color.WhiteSmoke;
+            cuiButton2.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton2.PressedImageTint = Color.White;
+            cuiButton2.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.Rounding = new Padding(30);
+            cuiButton2.Size = new Size(338, 79);
+            cuiButton2.TabIndex = 13;
+            cuiButton2.TextAlignment = StringAlignment.Far;
+            cuiButton2.TextOffset = new Point(0, 0);
+            cuiButton2.Click += button1_Click;
+            // 
             // Menu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RoyalBlue;
             ClientSize = new Size(1550, 900);
-            Controls.Add(pictureBox7);
-            Controls.Add(pictureBox6);
-            Controls.Add(pictureBox5);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
+            Controls.Add(cuiGradientPanel1);
             Controls.Add(btnFechar);
-            Controls.Add(gpbMenus);
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(3, 2, 3, 2);
             Name = "Menu";
             Text = "Form1";
             gpbMenus.ResumeLayout(false);
+            gpbMenus.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
@@ -259,6 +387,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            cuiGradientPanel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -268,7 +397,6 @@
         private Button btnServico;
         private Button btnPecaItem;
         private Button btnEquipamento;
-        private Button btnOrdemServico;
         private Button btnPessoa;
         private Label btnFechar;
         private Button button1;
@@ -282,5 +410,9 @@
         private PictureBox pictureBox5;
         private PictureBox pictureBox6;
         private PictureBox pictureBox7;
+        private CuoreUI.Controls.cuiButton btnOrdemServico;
+        private CuoreUI.Controls.cuiGradientPanel cuiGradientPanel1;
+        private CuoreUI.Controls.cuiButton cuiButton2;
+        private CuoreUI.Controls.cuiButton cuiButton1;
     }
 }
